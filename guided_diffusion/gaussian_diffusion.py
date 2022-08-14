71e32cf161f2ab17e6af5a649b803ae897478e07
Noah Cohn <noahc42@gmail.com>
2022-08-14 15:40:36 -0700

    Update gaussian_diffusion.py

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -38,6 +38,13 @@ def get_named_beta_schedule(schedule_name,num_diffusion_timesteps):
             num_diffusion_timesteps,
             lambda t: math.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2,
         )
+    if schedule_name == "1inkus":
+        scale=1000 / num_diffusion_timesteps
+        beta_start=scale * 0.0001
+        beta_end=scale * 0.02
+        return np.linspace(
+            beta_start,beta_end,num_diffusion_timesteps,dtype=np.float64
+        )
     else:
         raise NotImplementedError(f"unknown beta schedule: {schedule_name}")
 
