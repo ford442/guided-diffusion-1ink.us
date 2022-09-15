5eb64a082aa85886bd0b3a1dec661c86d42d46b5
Noah Cohn <noahc42@gmail.com>
2022-09-15 00:28:50 -0700

    Update unet.py

--- guided_diffusion/unet.py
+++ guided_diffusion/unet.py
@@ -284,7 +284,7 @@ class UNetModel(nn.Module):
         self.conv_resample=conv_resample
         self.num_classes=num_classes
         self.use_checkpoint=use_checkpoint
-        self.dtype=th.float16 if use_fp16 else th.float32
+        self.dtype=th.float32
         self.num_heads=num_heads
         self.num_head_channels=num_head_channels
         self.num_heads_upsample=num_heads_upsample
