4a34ba3a8f458c62b8282b20f78383feb8198e5a
Noah Cohn <noahc42@gmail.com>
2022-09-15 20:27:26 -0700

    Update gaussian_diffusion.py

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -17,17 +17,17 @@ def get_named_beta_schedule(schedule_name,num_diffusion_timesteps):
     elif schedule_name == "1inkus":
         scale=1000.0/num_diffusion_timesteps
         beta_start=scale*0.0001
-        beta_end=scale*0.0199
+        beta_end=scale*0.01951
         return np.linspace(beta_start,beta_end,num_diffusion_timesteps,dtype=np.float64)
     elif schedule_name == "1inkusLight":
         scale=1000.0/num_diffusion_timesteps
         beta_start=scale*0.0000979
-        beta_end=scale*0.0195
+        beta_end=scale*0.01951
         return np.linspace(beta_start,beta_end,num_diffusion_timesteps,dtype=np.float64)
     elif schedule_name == "1inkusDark":
         scale=1000.0/num_diffusion_timesteps
         beta_start=scale*0.000102
-        beta_end=scale*0.0195
+        beta_end=scale*0.01951
         return np.linspace(beta_start,beta_end,num_diffusion_timesteps,dtype=np.float64)
     elif schedule_name == "1inkusLite":
         scale=1000.0/num_diffusion_timesteps
